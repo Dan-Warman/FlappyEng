37c6254ef79d365586d6b08b3fb6c2d6cf9e50ac
Dan-Warman <danwarman14@gmail.com>
2016-06-29 18:38:20 +0100

    FlappyEng

---
+++ FlappyEngine/src/Main/FlappyEngine/Testing/Menu.java
@@ -0,0 +1,140 @@
+package Main.FlappyEngine.Testing;
+//By Dan 
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Menu {
+	
+	private static int WIDTH = 800;
+	private static int HEIGHT = 800;
+	private static String TITLE = "Menu";
+	
+	
+	public static void main(String[] args) throws IOException{
+		
+			ImageIcon icon = new ImageIcon("src/Main/FlappyEngine/Imgs/icon.png");
+			
+			final BufferedImage controlstext = ImageIO.read(new File("src/Main/FlappyEngine/Imgs/controlstext.png"));
+		
+			playsound();
+		 	JFrame frame = new JFrame();
+		 	frame.setSize(WIDTH, HEIGHT);
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    frame.getContentPane().setLayout(null);
+		    frame.setVisible(true);
+		    frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		    frame.setResizable(false);
+		    frame.setTitle(TITLE);
+		    frame.setIconImage(icon.getImage());
+		    
+			JLabel controllabl = new JLabel(new ImageIcon(controlstext));
+			controllabl.setBounds(200, 200, 400,400);
+			frame.getContentPane().add(controllabl);
+			controllabl.setVisible(false);
+		    
+		    JButton controls = new JButton("Controls");
+		    controls.setBounds(300, 150,200,23);
+		    frame.getContentPane().add(controls);
+		    controls.setBackground(Color.pink);
+		    
+		    controls.addMouseListener(new MouseAdapter(){
+		    	public void mouseEntered(MouseEvent e){
+		    		controls.setBackground(Color.pink.darker());
+		    	}
+		    	public void mouseExited(MouseEvent e){
+		    		controls.setBackground(Color.pink);
+		    		
+		    	}
+		    	public void mouseReleased(MouseEvent e){
+		    		if(e.getButton() == 1){
+		    			//System.out.println("isWorking");
+		    			frame.getContentPane().add(controllabl);
+		    			controllabl.setVisible(true);
+		    		}
+		    		if(e.getButton() == 3){
+		    			frame.getContentPane().add(controllabl);
+		    			controllabl.setVisible(false);
+		    		}
+		    		    		
+		    	}
+		    	
+		    	
+		    });
+		    
+		    JButton exit = new JButton("Exit");
+		    exit.setBounds(300,200,200,23);
+		    frame.getContentPane().add(exit);
+		    exit.setBackground(Color.pink);
+		    
+		    exit.addMouseListener(new MouseAdapter(){
+		    	public void mouseEntered(MouseEvent e){
+		    		exit.setBackground(Color.pink.darker());
+		    	}
+		    	public void mouseExited(MouseEvent e){
+		    		exit.setBackground(Color.pink);
+		    		
+		    	}
+		    	public void mouseReleased(MouseEvent e){
+		    		if(e.getButton() == 1){
+		    			frame.dispose();
+		    		}
+		    	}
+		    });
+		    
+		    JLabel credits = new JLabel();
+		    
+		    credits.setText("By Dan Warman");
+		    credits.setBounds(10, 740, 200, 25);
+		    frame.getContentPane().add(credits);
+
+		    JButton start = new JButton("Start");
+		    start.setBounds(300, 100, 200, 23);
+		    frame.getContentPane().add(start);
+		    start.setBackground(Color.pink);
+
+		    
+		    start.addMouseListener(new MouseAdapter() {
+		    	public void mouseEntered(MouseEvent e){
+		    		start.setBackground(Color.pink.darker());
+		    	}
+		    	public void mouseExited(MouseEvent e){
+		    		start.setBackground(Color.pink);
+		    		
+		    	}
+		        public void mouseReleased(MouseEvent e){
+		            if ( e.getButton() == 1 ){ // 1-left, 2-middle, 3-right button
+		                FlappyBird.main(args);
+		                frame.dispose();
+		            }
+		        }
+		    });
+		    
+
+	}
+    public static void playsound() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("scr/Main/FlappyEngine/Sound/soundtrack.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+}
+//By Dan 
---
+++ FlappyEngine/src/Main/FlappyEngine/Testing/Renderer.java
@@ -0,0 +1,27 @@
+package Main.FlappyEngine.Testing;
+//By Dan Warman
+
+import java.awt.Graphics;
+import java.io.IOException;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		try {
+			FlappyBird.flappyBird.repaint(g);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+//By Dan Warman
